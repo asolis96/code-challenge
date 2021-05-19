c58f7d33adb9d70deb13bc0b790a3e6f5eee3c1e
Adrian Solis <adrian.solis@unosquare.com>
2021-05-19 14:22:37 -0500

    Challenge 1

---
+++ CodeChallenge1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckEnding("abc", "bC"));
+            Console.WriteLine(CheckEnding("abc", "d"));
+            Console.WriteLine(CheckEnding("samurai", "zi"));
+            Console.WriteLine(CheckEnding("feminine", "nine"));
+            Console.WriteLine(CheckEnding("convention", "tio"));
+
+            Console.ReadKey();
+        }
+
+        public static bool CheckEnding(string s1, string s2)
+        {
+            return s1.ToLower().EndsWith(s2.ToLower());
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Code Challenge
+
+Adrian Solis
